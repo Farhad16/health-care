afd53666a37e57b3dee8ad0944cdc07741a2960b
unknown <fhossain389@gmail.com>
2020-07-02 13:54:12 +0600

    port change

--- health/settings.py
+++ health/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '8mg$u2=8j3y8$g3-po-1xsh&g_!eaw!%+kk(m(mxl9ndphv4wf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://health-care1.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://health-care1.herokuapp.com/',
+                 'http://127.0.0.1:8000/']
 
 
 # Application definition
